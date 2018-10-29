6709996d2a246d44537d17e40fb29e5db134ff87
Beor18 <fer.ragnar19@gmail.com>
2018-10-29 09:20:05 -0300

    inyectar servicio

--- demo01/src/app/user.service.ts
+++ demo01/src/app/user.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(protected http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get('https://randomuser.me/api/?results=25');
+  }
+
 }
