a80790216e09bec35d0d99756738e23209143689
Beor18 <fer.ragnar19@gmail.com>
2018-10-29 09:26:45 -0300

    inyectar servicio

--- demo01/src/app/app.component.ts
+++ demo01/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo01';
+  users: any[] = [];
+
+  constructor(protected userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.getUsers()
+    .subscribe(
+      (data) => {
+        this.users = data['results'];
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
