40d7a1015e2e4a7581be7fa39a39a38c9f441128
Beor18 <fer.ragnar19@gmail.com>
2018-11-19 09:36:53 -0300

    form

--- demo02/src/app/app.component.ts
+++ demo02/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'demo02';
+  user: FormGroup;
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.user = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+        password: ['', Validators.required],
+        passwordRepeat: ['', Validators.required]
+    });
+  }
 }
